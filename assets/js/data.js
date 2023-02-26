08683b10208cda4aee9480c0a695e0c1a4b3ac2b
gabycrem <gabymacre@gmail.com>
2023-02-26 00:29:24 -0300

    creando arch data.js para carga dinamica de datos

---
+++ assets/js/data.js
@@ -0,0 +1,97 @@
+let data = {
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Front End Developer",
+        "first": "Jennie",
+        "last": "Nichols"
+      },
+      "location": {
+        "street": {
+          "number": 8929,
+          "name": "Valwood Pkwy"
+        },
+        "city": "Billings",
+        "state": "Michigan",
+        "country": "United States",
+        "postcode": "63104"
+      },
+      "email": "jennie.nichols@example.com",
+      "webSite": "www.anaalvarezdeveloper.com",
+      "dob": {
+        "date": "1992-03-08",
+        "age": 30
+      },
+      "phone": "(272) 790-0888",
+      "cell": "(489) 330-2385",
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/75.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "aboutMe": "Programadora con más de 4 años de experiencia en agencias de publicidad. Especializada en aplicaciones móviles para Android y iOS. Conocimientos de Java, JavaScript y Angular. En mi actual puesto recientemente creé una app para una empresa de moda que cuenta con más de 20 mil descargas y ha recibido una calificación de 4.5 estrellas. Deseo utilizar mis capacidades para colaborar en los proyectos de SinTech y generar aplicaciones de alta calidad y eficiencia.",
+      "experience": [
+        {
+          "position": "Desarrolladora Front-End",
+          "place": "124 MediaCorp",
+          "startDate": "Enero 2019",
+          "finishDate": "Actualidad",
+          "jobDescrition": [
+            "Participé en el desarrollo de 3 nuevas aplicaciones y 2 páginas web para los clientes de la empresa.",
+            "Colaboré con diseñadores, copywriters e investigadores UX para brindar la mejor experiencia a los usuarios de las apps y sitios web.",
+            "Reduje en un 10% los errores presentados en la app Zaza Ballet, gracias a mis habilidades de resolución de problemas.",
+            "Implementé una tienda online para la página web TheOutnet, la cual elevó las ventas en un 15%."
+          ]
+        },
+        {
+          "position": "Programadora Jr",
+          "place": "Antlers Inc",
+          "startDate": "Febrero 2017",
+          "finishDate": "Diciembre 2018",
+          "jobDescrition": [
+            "Brindé mantenimiento continuo a las aplicaciones desarrolladas en JavaScript.",
+            "Realicé pruebas de usuario periódicas reduciendo el número de bugs en un 15%.",
+            "Desarrollé JavaScript para modificación de interfaz e integración con sistemas ERP, CRM, OCR, DMS.",
+            "Recibí una calificación de 9,2 en mi evaluación de desempeño anual por parte del coordinador del departamento."
+          ]
+        }
+      ],
+      "education": {
+        "title": "Grado en Ingeniería del Software",
+        "place": "Facultad de Informática de Barcelona",
+        "startDate": "Septiembre 2013",
+        "finishDate": "Junio 2017"
+      },
+      "softSkill": [
+        "Usabilidad",
+        "Trabajo en equipo",
+        "Microsoft FrontPage",
+        "Drupal",
+        "Angular",
+        "GIT",
+        "Apache Ant",
+        "Visual Basic",
+        "SQL Server",
+        "Prácticas Agiles",
+        "Resolución de problemas",
+        "Wordpress"
+      ],
+      "programLanguages": [
+        "JavaScript",
+        "Java",
+        "HTML",
+        "CSS"
+      ],
+      "languages": {
+        "language": "Inglés",
+        "lvl": "B2"
+      },
+      "addCourses": {
+        "title": "JavaScript Bootcamp",
+        "place": "Barcelona CodeSchool",
+        "date": "2019"
+      }
+    }
+  ]
+}
